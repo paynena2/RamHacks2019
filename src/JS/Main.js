83e5f979e4cfbcc0a9cd037d0c3e963e87b6064c
Nathaniel Payne <paynena2@mymail.vcu.edu>
2019-09-28 19:35:03 -0400

    Quicksaving...

---
+++ src/JS/Main.js
@@ -0,0 +1,5 @@
+var para = document.createElement("p")
+var node = document.createTextNode("This is new.")
+para.appendChild(node)
+var element = document.getElementById("deposits")
+element.appendChild(para)
