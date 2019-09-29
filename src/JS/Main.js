0129e5d839e0928ed32f60748d3cc7d3f39cdf6c
Cameron Duncan <cammdun@outlook.com>
2019-09-29 00:06:07 -0400

    added connection to main, and created arrays to store data

--- src/JS/Main.js
+++ src/JS/Main.js
@@ -1,5 +1,68 @@
-function populateData(){
+var mysql = require('mysql');
+    
+var con = mysql.createConnection({
+  host: "hackathon.crfggblwxxhg.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "cupcake123",
+  database: "Hackathondb"
+});
 
+function populateDeposit(){
+    var arr = new array();
+
+    var mysql = require('mysql');
+    
+    var con = mysql.createConnection({
+      host: "hackathon.crfggblwxxhg.us-east-1.rds.amazonaws.com",
+      user: "admin",
+      password: "cupcake123",
+      database: "Hackathondb"
+    });
+    
+    con.connect(function(err) {
+      
+      con.query("SELECT id, date, amount, payee-id, status FROM Depositstb", function (err, result, fields) {
+        
+        console.log(result);
+        arr.push(result);
+      });
+    });
+}
+
+function populateWithdrawal(){
+    con.connect(function(err) {
+        
+        con.query("SELECT id, date, amount, payee-id, status FROM Withdrawaltb", function (err, result, fields) {
+         
+          console.log(result);
+          var arr2 = new array();
+          arr2.push(result);
+        });
+    });
+}
+
+function populateBills(){
+    con.connect(function(err) {
+       
+        con.query("SELECT id, creation-date, payment-date, amount, payee-id, status FROM Bills", function (err, result, fields) {
+         
+          console.log(result);
+          var arr3 = new array();
+          arr3.push(result);
+        });
+    });
+}
+
+function populateTransfers(){
+    con.connect(function(err) {
+        
+        con.query("SELECT id, date, amount, payee-id, status FROM Transfer", function (err, result, fields) {
+          
+          console.log(result);
+          var arr4 = new array();
+          arr4.push(result);
+        });
+    });
 }
 
 function setHTML(deposits, withdrawals, bills, transfers, merchants, loans){
