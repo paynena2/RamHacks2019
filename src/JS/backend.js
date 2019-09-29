bdc7acd4ee46dc39a4d04f3819e9a8150d3c5cbc
Nathaniel Payne <paynena2@mymail.vcu.edu>
2019-09-28 22:40:06 -0400

    Fixed spelling

--- src/JS/backend.js
+++ src/JS/backend.js
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   //if (err) throw err;
-  con.query("SELECT id, date, amount, peyee-idr, status FROM Depositstb", function (err, result, fields) {
+  con.query("SELECT id, date, amount, payee-id, status FROM Depositstb", function (err, result, fields) {
     //if (err) throw err;
     console.log(result);
   });
