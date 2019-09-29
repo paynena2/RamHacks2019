22809d485abc288cc44845bb7be97d0234b489a6
Nathaniel Payne <paynena2@mymail.vcu.edu>
2019-09-28 20:30:27 -0400

    Added overview numbers for each tab

--- src/JS/Main.js
+++ src/JS/Main.js
@@ -1,5 +1,20 @@
-var para = document.createElement("p")
-var node = document.createTextNode("This is new.")
-para.appendChild(node)
-var element = document.getElementById("deposits")
-element.appendChild(para)
+function populateData(){
+
+}
+function setHTML(deposits, withdrawals, bills, transfers, merchants, loans){
+    document.getElementById("deposit_num").innerHTML = "$" + deposits
+    document.getElementById("with_num").innerHTML = "$" + withdrawals
+    document.getElementById("bills_num").innerHTML = "$" + bills
+    document.getElementById("transfer_num").innerHTML = "$" + transfers
+    document.getElementById("merchant_num").innerHTML = "$" + merchants
+    document.getElementById("loan_num").innerHTML = "$" + loans
+}
+
+let deposits = 0
+let withdrawals = 0
+let bills = 0
+let transfers = 0
+let merchants = 0
+let loans = 0
+
+setHTML(deposits, withdrawals, bills, transfers, merchants, loans)
